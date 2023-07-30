5d5d010e1ec14523f886bbe7873fd1d43f9e906f
Zubair <zubair.maqsood6@gmail.com>
2023-07-30 17:14:30 +0100

    debugging now

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,8 +1,8 @@
-import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
-import crypto from "crypto";
+// import { Request, Response, NextFunction } from "express";
+// import jwt from "jsonwebtoken";
+// import crypto from "crypto";
 
-const SECRET_KEY = crypto.randomBytes(32).toString("hex");
+// const SECRET_KEY = crypto.randomBytes(32).toString("hex");
 
 // async function generateJWT(email: string, password: string) {
 //   const options = {
@@ -18,23 +18,23 @@ const SECRET_KEY = crypto.randomBytes(32).toString("hex");
 //   }
 // }
 
-export const authenticateToken = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+// export const authenticateToken = (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction,
+// ) => {
+//   const authHeader = req.headers["authorization"];
+//   const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
+//   if (!token) {
+//     return res.status(401).json({ message: "Unauthorized" });
+//   }
 
-  jwt.verify(token, SECRET_KEY, (err, user) => {
-    if (err) {
-      return res.status(403).json({ message: "Forbidden" });
-    }
-    req.user = user;
-    next();
-  });
-};
+//   jwt.verify(token, SECRET_KEY, (err, user) => {
+//     if (err) {
+//       return res.status(403).json({ message: "Forbidden" });
+//     }
+//     req.user = user;
+//     next();
+//   });
+// };
--- src/resources/admin/admin.controller.ts
+++ src/resources/admin/admin.controller.ts
@@ -1,8 +1,21 @@
 import { DynamoDB, S3 } from "aws-sdk";
-import { authenticateToken } from "../../middleware/middleware";
-import { Router, Request, Response, NextFunction } from "express";
+// import { authenticateToken } from "../../middleware/middleware";
+import { Router, Request, Response } from "express";
 import Controller from "../../utils/interfaces/controller.interface";
 import AdminService from "./admin.service";
+
+interface SearchFilter {
+	gender?: string;
+	city?: string;
+	age?: number;
+	religion?: string;
+	ethnicity?: string;
+	height?: number;
+	hasChildren?: boolean;
+	wantChildren?: boolean;
+	profession?: string;
+	education?: string;
+  }
 
 export default class AdminController implements Controller {
   public path = "/user";
@@ -16,93 +29,90 @@ export default class AdminController implements Controller {
 
   initialiseRoutes(): void {
     //protected route
-    this.router.get(`${this.path}/:userId`, authenticateToken, this.getUser);
-
-    // // protected route
-    // this.router.get(
-    //   `${this.path}/allUsers`,
-    //   authenticateToken,
-    //   this.getAllUsers,
-    // );
+    this.router.get(`${this.path}/:userId`, this.getUser);
 
     // protected route
-    this.router.get(`${this.path}/search`, authenticateToken, this.search);
+    this.router.get(`${this.path}/allUsers`, this.getAllUsers);
+
+    // protected route
+    this.router.post(`${this.path}/search`, this.search);
 
     // protected route
     this.router.get(
       `${this.path}/:userId/approveApplication`,
-      authenticateToken,
       this.approveApplication,
     );
   }
 
-  getUser = async (
-    req: Request,
-    res: Response,
-  ): Promise<Response | void> => {
+  getUser = async (req: Request, res: Response): Promise<Response | void> => {
     const { userId } = req.params as { userId: string };
 
+    const user = await this.adminService.getSingleUser(userId);
 
-      const user = await this.adminService.getSingleUser(userId);
-
-      if (user) {
-        res.status(200).send({
-          user,
-        });
-      } else {
-        res.status(404).json({
-          message: "User not found",
-        });
-      }
+    if (user) {
+      res.status(200).send({
+        user,
+      });
+    } else {
+      res.status(404).json({
+        message: "User not found",
+      });
+    }
   };
 
-//   getAllUsers = async (
-//     req: Request,
-//     res: Response,
-//     next: NextFunction,
-//   ): Promise<Response | void> => {};
-
-  search = async (
+  getAllUsers = async (
     req: Request,
     res: Response,
   ): Promise<Response | void> => {
-    const {
-      gender,
-      city,
-      age,
-      religion,
-      ethnicity,
-      height,
-      hasChildren,
-      wantChildren,
-      profession,
-      education,
-    } = req.body;
-
-    // Construct a search filter based on the provided parameters
-    const searchFilter: Record<string, any> = {};
-
-    if (gender) searchFilter.gender = gender;
-    if (city) searchFilter.city = city;
-    if (age) searchFilter.age = age;
-    if (religion) searchFilter.religion = religion;
-    if (ethnicity) searchFilter.ethnicity = ethnicity;
-    if (height) searchFilter.height = height;
-    if (hasChildren) searchFilter.hasChildren = hasChildren;
-    if (wantChildren) searchFilter.wantChildren = wantChildren;
-    if (profession) searchFilter.profession = profession;
-    if (education) searchFilter.education = education;
-
     try {
-      // Call the adminService method to search for users based on the filter
-      const searchResults = await this.adminService.searchUsers(searchFilter);
-
-      return res.status(200).json({ searchResults });
+      const result = this.adminService.getAllUsers();
+      return res.status(200).json(result);
     } catch (error) {
-      console.error("Error searching users:", error);
+      console.error("Error fetching users:", error);
       return res.status(500).json({ message: "Failed to perform the search" });
     }
   };
+
+  search = async (req: Request, res: Response): Promise<Response | void> => {
+	const {
+	  gender,
+	  city,
+	  age,
+	  religion,
+	  ethnicity,
+	  height,
+	  hasChildren,
+	  wantChildren,
+	  profession,
+	  education,
+	} = req.body;
+
+	// Construct a search filter based on the provided parameters
+	const searchFilter: SearchFilter = {};
+
+	if (gender) searchFilter.gender = gender;
+	if (city) searchFilter.city = city;
+	if (age) searchFilter.age = age;
+	if (religion) searchFilter.religion = religion;
+	if (ethnicity) searchFilter.ethnicity = ethnicity;
+	if (height) searchFilter.height = height;
+	if (hasChildren) searchFilter.hasChildren = hasChildren;
+	if (wantChildren) searchFilter.wantChildren = wantChildren;
+	if (profession) searchFilter.profession = profession;
+	if (education) searchFilter.education = education;
+
+	try {
+	  // Call the adminService method to search for users based on the filter
+	  const searchResults = await this.adminService.searchUsers(searchFilter);
+
+	  return res.status(200).json({ searchResults });
+	} catch (error) {
+	  console.error("Error searching users:", error);
+	  return res.status(500).json({ message: "Failed to perform the search" });
+	}
+  }
+
+
 
   approveApplication = async (
     req: Request,
